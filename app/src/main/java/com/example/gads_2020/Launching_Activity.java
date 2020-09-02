2413525026cf18a052f181817423106e12b7aad8
MICHAEL LOUPA <michaelloupa@gmail.com>
2020-09-02 22:36:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gads_2020/Launching_Activity.java
@@ -0,0 +1,35 @@
+package com.example.gads_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Launching_Activity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launching_);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewpageradapter pagerAdapter = new viewpageradapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/example/gads_2020/viewpageradapter.java
@@ -0,0 +1,49 @@
+package com.example.gads_2020;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class viewpageradapter extends FragmentPagerAdapter {
+    public viewpageradapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    public viewpageradapter(FragmentManager supportFragmentManager) {
+        super(supportFragmentManager);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new learning_leaders();
+            case 1:
+                return new skill_iq();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        switch (position) {
+            case 0:
+                return "Learning Leaders";
+            case 1:
+                return "Skill IQ Leaders";
+        } return super.getPageTitle(position);
+    }
+}
+
