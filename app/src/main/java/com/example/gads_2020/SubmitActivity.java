92e043ba13d470d495e3b90fc2b3ba40b7f2dcfd
MICHAEL LOUPA <michaelloupa@gmail.com>
2020-09-09 01:03:24 +0300

    first commit

--- app/src/main/java/com/example/gads_2020/SubmitActivity.java
+++ app/src/main/java/com/example/gads_2020/SubmitActivity.java
@@ -2,13 +2,26 @@ package com.example.gads_2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SubmitActivity extends AppCompatActivity {
+
+    private ImageButton backstack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+
+        ImageButton backstack = findViewById(R.id.backstack);
+        backstack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SubmitActivity.this, LaunchingActivity.class));
+            }
+        });
     }
 }
